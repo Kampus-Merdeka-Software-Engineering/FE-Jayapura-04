db0fad3aa8dea90147add93c713783888d484e43
Sigit Arif Anggoro <sigit.arifanggoro@advotics.com>
2023-10-07 17:14:25 +0700

    move function out

--- JS/pakaianbe.js
+++ JS/pakaianbe.js
@@ -17,77 +17,78 @@ fetch(idProduk)
   .then(product => {
     // Menampilkan data produk di konsol
     console.log('Data Produk:', product);
-
-    // Membuat innerHTML dari data produk
-    function createProductElement(product) {
-      const deskripsi = document.createElement("div");
-      deskripsi.innerHTML = `
-        <div class="detail-produk" id="detail-produk">
-          <div class="produk-img">
-            <img src="${product.img}" alt="Baju" id="besarImg" />
-          </div>
-          <div class="content">
-            <h3 id="nama-produk">${product.name}</h3>
-            <h4 id="harga-produk">${product.price}</h4>
-            <h3>Warna</h3>
-            <div class="warna-img">
-              ${createColorOptions(product.colors)}
-            </div>
-            <div>
-              <select id="ukuranbaju" >
-                ${createSizeOptions(product.sizes)}
-              </select>
-              <input
-                type="number"
-                value="1"
-                id="jumlah-produk"
-                min="1"
-                max="10"
-                onkeydown="return false;"
-              />
-            </div>
-          //   <h3>Deskripsi</h3>
-          //   <p>
-          //     <span id="selengkapnya" onclick="tambahUlasan()">selengkapnya...</span>
-          //   </p>
-          //   <div class="belanja">
-          //     <a href="#" class="cta">Belanja Sekarang</a>
-          //     <a href="#" class="cta" id="tambah-keranjang" onclick="tambahKeranjang()">Tambah Keranjang</a>
-          //   </div>
-          // </div>
-        </div>`;
-      
-      return deskripsi;
-    }
-
-    function createSizeOptions(sizes) {
-      let options = "";
-      sizes.forEach((size) => {
-        options += `<option value="${size}">${size}</option>`;
-      });
-      return options;
-    }
-
-    function createColorOptions(colors) {
-      let imgcolor = "";
-      colors.forEach((color) => {
-        imgcolor += `<img
-          src="${color}"
-          alt="Baju"
-          class="kecilImg"
-          onclick="tampilGambar()"/>`;
-      });
-      return imgcolor;
-    }
-
-    document.addEventListener("DOMContentLoaded", function () {
-      const productElement = createProductElement(product);
-      const productContainer = document.getElementById("deskripsi-produk");
-      productContainer.appendChild(productElement);
-    });
+    createProductElement(product);
   })
   .catch((error) => {
     console.error('Ada kesalahan saat mengambil data produk:', error);
+  });
+
+  // Membuat innerHTML dari data produk
+  function createProductElement(product) {
+    const deskripsi = document.createElement("div");
+    deskripsi.innerHTML = `
+      <div class="detail-produk" id="detail-produk">
+        <div class="produk-img">
+          <img src="${product.img}" alt="Baju" id="besarImg" />
+        </div>
+        <div class="content">
+          <h3 id="nama-produk">${product.name}</h3>
+          <h4 id="harga-produk">${product.price}</h4>
+          <h3>Warna</h3>
+          <div class="warna-img">
+            ${createColorOptions(product.colors)}
+          </div>
+          <div>
+            <select id="ukuranbaju" >
+              ${createSizeOptions(product.sizes)}
+            </select>
+            <input
+              type="number"
+              value="1"
+              id="jumlah-produk"
+              min="1"
+              max="10"
+              onkeydown="return false;"
+            />
+          </div>
+        //   <h3>Deskripsi</h3>
+        //   <p>
+        //     <span id="selengkapnya" onclick="tambahUlasan()">selengkapnya...</span>
+        //   </p>
+        //   <div class="belanja">
+        //     <a href="#" class="cta">Belanja Sekarang</a>
+        //     <a href="#" class="cta" id="tambah-keranjang" onclick="tambahKeranjang()">Tambah Keranjang</a>
+        //   </div>
+        // </div>
+      </div>`;
+    
+    return deskripsi;
+  }
+
+  function createSizeOptions(sizes) {
+    let options = "";
+    sizes.forEach((size) => {
+      options += `<option value="${size}">${size}</option>`;
+    });
+    return options;
+  }
+
+  function createColorOptions(colors) {
+    let imgcolor = "";
+    colors.forEach((color) => {
+      imgcolor += `<img
+        src="${color}"
+        alt="Baju"
+        class="kecilImg"
+        onclick="tampilGambar()"/>`;
+    });
+    return imgcolor;
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const productElement = createProductElement(product);
+    const productContainer = document.getElementById("deskripsi-produk");
+    productContainer.appendChild(productElement);
   });
 
 
