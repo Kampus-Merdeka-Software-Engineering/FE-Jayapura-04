dc922533e1776f69af16a2b0d3433ca58f449756
afifahzahro <142975676+afifahzahro@users.noreply.github.com>
2023-10-08 14:41:13 +0700

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -226,7 +226,7 @@ function tambahKeranjang() {
   const totalHargaProduk = hargaProduk * jumlahProduk;
 
   const produkSama = keranjangBelanja.find(
-    (produk) => produk.nama === namaProduk
+    (produk) => produk.nama === namaProduk && produk.gambar === gambarProduk
   );
 
   if (produkSama) {
@@ -301,119 +301,4 @@ function updateKeranjangBelanja() {
   return updateKeranjangBelanja;
 }
 
-//fungsi tambah ulasan
 
-function tambahUlasan() {
-  let selengkapnya = document.querySelector(".form-ulasan");
-  selengkapnya.classList.toggle("active");
-
-  document.getElementById("edit").onclick = () => {
-    editUlasan.classList.toggle("active");
-  };
-}
-function tutupUlasan() {
-  let cancelulasan = document.querySelector(".form-ulasan");
-  cancelulasan.classList.remove("active");
-}
-
-// Mengambil semua elemen bintang
-const stars = document.querySelectorAll(".star");
-
-// Menggunakan event listener untuk setiap bintang
-stars.forEach((star, index) => {
-  star.addEventListener("click", () => {
-    // Mengatur semua bintang sebelumnya menjadi abu
-    stars.forEach((s, i) => {
-      if (i <= index) {
-        s.style.color = "#ffdd00"; // Warna bintang yang diklik
-      } else {
-        s.style.color = "gray"; // Warna bintang yang belum diklik
-      }
-    });
-
-    // Memasukkan nilai rating yang diklik ke input
-    const ratingInput = document.querySelector('input[name="rating"]');
-    ratingInput.value = index + 1; // Index dimulai dari 0, jadi ditambah 1
-  });
-});
-
-// Mengambil elemen form ulasan
-const formUlasan = document.getElementById("form-ulasan");
-const tombolSubmit = document.getElementById("submit-ulasan");
-
-// Mengambil elemen ulasan produk
-const ulasanProduk = document.getElementById("ulasan-display");
-
-// Mengikat event submit pada form ulasan
-formUlasan.addEventListener("submit", function (event) {
-  event.preventDefault(); // Mencegah pengiriman formulir secara default
-
-  // Mengambil nilai username, rating, dan ulasan yang diisi oleh pengguna
-  const usernameInput = document.getElementById("username");
-  const ulasanInput = document.getElementById("opinion");
-  const ratingInput = document.querySelector('input[name="rating"]:checked');
-
-  // Mengekstrak nilai dari elemen-elemen input
-  const username = usernameInput.value;
-  const ulasan = ulasanInput.value;
-  const ratingValue = ratingInput ? parseInt(ratingInput.value) : 0;
-
-  console.log("Username:", username);
-  console.log("Rating:", ratingValue);
-  console.log("Ulasan:", ulasan);
-  // Mengosongkan input setelah pengguna mengirim ulasan
-  usernameInput.value = "";
-  ulasanInput.value = "";
-  // Mengosongkan input rating
-  const radioButtons = document.querySelectorAll('input[name="rating"]');
-  radioButtons.forEach((radio) => {
-    radio.checked = false;
-  });
-  // Mengatur semua bintang menjadi abu setelah mengirim ulasan
-  stars.forEach((star) => {
-    star.style.color = "gray";
-  });
-  // Menyembunyikan form setelah pengguna mengirim ulasan
-  const formUlasan = document.getElementById("form-ulasan");
-  //formUlasan.style.display = "none";
-  // Buat elemen untuk menampilkan ulasan
-  const ulasanItem = document.getElementById("ulasan-produk");
-  //ulasanItem.classList.add("ulasan-produk");
-
-  // Buat elemen-elemen untuk menampilkan username, rating, dan ulasan
-  const usernameElement = document.getElementById("nama-user");
-  const ratingElement = document.getElementById("bintang-kecil");
-  const ulasanTekstualElement = document.getElementById("opini-user");
-  const garisHorizontal = document.getElementById("horizontal");
-
-  // Membuat gambar bintang berdasarkan nilai rating
-  let ratingStars = "";
-  for (let i = 1; i <= 5; i++) {
-    if (i <= ratingValue) {
-      ratingStars +=
-        '<img src="img/bintang.png" alt="Filled Star" class="bintang">';
-    } else {
-      ratingStars +=
-        '<img src="img/bintangkosong.png" alt="Empty Star" class="bintangkosong">';
-    }
-  }
-
-  // Menetapkan nilai ke elemen-elemen yang dibuat
-  usernameElement.innerHTML = `<div class="nama-user" id="nama-user"><h5>${username}</h5><a href="#" id="edit" onclick="gantiUlasan()">Edit</a></div> `;
-  ratingElement.innerHTML = ratingStars;
-  ulasanTekstualElement.innerHTML = `<p>${ulasan}</p>`;
-  garisHorizontal.innerHTML = `<div class ="horizontal" id="horizontal"></div>`;
-
-  // Menambahkan elemen-elemen ke ulasanItem
-  ulasanItem.appendChild(usernameElement);
-  ulasanItem.appendChild(ratingElement);
-  ulasanItem.appendChild(ulasanTekstualElement);
-  ulasanItem.appendChild(garisHorizontal);
-
-  // Menambahkan ulasanItem ke ulasanProduk
-  ulasanProduk.appendChild(ulasanItem);
-});
-function gantiUlasan() {
-  let editulasan = document.querySelector(".form-ulasan");
-  editulasan.classList.toggle("active");
-}
